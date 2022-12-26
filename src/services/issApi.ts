ef2dc03e1ca71c35e452dce94c82243148ccd591
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-26 17:47:51 +0300

    added vercel config

--- src/services/issApi.ts
+++ src/services/issApi.ts
@@ -5,7 +5,7 @@ import { IssLocation, PeopleInSpace } from "../types/types";
 export const IssApi = createApi({
   reducerPath: "IssLocationApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/",
+    baseUrl: "http://api.open-notify.org/",
   }),
   endpoints: (builder) => ({
     getIssLocation: builder.query<IssLocation, string>({
