e89c07b1fae0dd83b6b1b9ec18486e7934224ae3
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-26 16:36:08 +0300

    fixed api

--- src/services/issApi.ts
+++ src/services/issApi.ts
@@ -5,7 +5,7 @@ import { IssLocation, PeopleInSpace } from "../types/types";
 export const IssApi = createApi({
   reducerPath: "IssLocationApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api/",
+    baseUrl: "http://api.open-notify.org/",
   }),
   endpoints: (builder) => ({
     getIssLocation: builder.query<IssLocation, string>({
