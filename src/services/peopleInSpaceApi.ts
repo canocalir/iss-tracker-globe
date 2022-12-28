2970e3379689e6fd97699894971304736fd043cd
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-28 18:20:30 +0300

    fixed toml

--- src/services/peopleInSpaceApi.ts
+++ src/services/peopleInSpaceApi.ts
@@ -4,7 +4,7 @@ import { PeopleInSpace } from "../types/types";
 export const PeopleInSpaceApi = createApi({
     reducerPath: 'InSpaceApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: `https://www.howmanypeopleareinspacerightnow.com/`,
+        baseUrl: `/people`,
     }),
     endpoints: (builder) => ({
         getPeopleInSpace: builder.query<PeopleInSpace,string>({
